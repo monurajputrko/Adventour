33d5e371785c8ce9374b05ba1d64a4f0f42a4782
karnking <karankotai9183@gmail.com>
2023-08-09 19:38:03 +0530

    1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+
 import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from '@auth0/auth0-react';
+
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
+import theme from "./theme"
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
+import { App } from './App';
+import { BrowserRouter } from 'react-router-dom';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <Auth0Provider
+      domain="dev-fxtuxl0mqujeteaf.us.auth0.com"
+      clientId="QWHRQSlRrsoV97IM3V9mtLhicZAU9q4M"
+      // clientId="DIz7yAvJoZObjH7DlJUcHSzj1r5kL24o"
+      authorizationParams={{
+        redirect_uri: window.location.origin
+      }}
+    >
+      <BrowserRouter>
+        <ChakraProvider>
+        <App />
+        </ChakraProvider>
+      </BrowserRouter>
+    </Auth0Provider>
+  </Provider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
