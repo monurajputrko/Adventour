a4f6bee3c3a466ecb744e39419cb849baf227121
karnking <karankotai9183@gmail.com>
2023-08-09 14:57:00 +0530

    secondary navbar

--- src/HomePage/Allroutes.jsx
+++ src/HomePage/Allroutes.jsx
@@ -3,20 +3,16 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './Home'
 import Login from "../Components/Login/Login"
 import Listing from '../Components/Bookings/Listing'
-import { ThemeProvider, createMuiTheme } from "@mui/material"
+import { ThemeProvider } from "@mui/material"
 import { muiTheme } from '../App'
 
 const Allroutes = () => {
-
-
     return (
-       
         <Routes>
             <Route path={"/"} element={<ThemeProvider theme={muiTheme}><Home/></ThemeProvider>} ></Route>
             <Route path={"/login"} element={<Login/>} ></Route>
             <Route path={"/booking"} element={<Listing />} />
         </Routes>
-  
   )
 }
 
