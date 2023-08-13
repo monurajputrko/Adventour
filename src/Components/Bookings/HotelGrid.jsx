ff2f3d0600d037e757c588ad1fd1bf45261a18e5
karnking <karankotai9183@gmail.com>
2023-08-13 14:27:29 +0530

    filter and grid responsive

--- src/Components/Bookings/HotelGrid.jsx
+++ src/Components/Bookings/HotelGrid.jsx
@@ -8,17 +8,24 @@ import Filters from './Filters'
 
 const HotelGrid = () => {
     const dispatch = useDispatch()
+    const [spinner, setSpinner] = useState(false)
     const hotels = useSelector(store => store.hotels.hotels) || []
     const isLoading = useSelector(store => store.hotels.isLoading)
+    const showSpinner = () => {
+        setSpinner(true)
+        setTimeout(() => {
+            setSpinner(false)
+        }, 1000)
+    }
     useEffect(() => {
         dispatch(getHotel())
     }, [])
     if (isLoading) return <Loader />
-    return <Flex mx="20px" w="80vw" m="auto" >
-        <Filters />
-        <Grid p="10px 2em 10px 10px" w="80vw" ml="20vw">
+    return <Flex mx="20px" w={{base:"98vw", lg:"80vw"}} m="auto" flexDirection={{base:"column",lg:"row"}}>
+        <Filters showSpinner={showSpinner}/> 
+        <Grid p={{base:"10px",lg:"10px 2em 10px 10px"}} w={{base:"95vw",lg:"70vw"}} ml={{base:"30px",lg:"22vw"}}>
             {hotels?.map(hotel => {
-                return <HotelsComp key={hotel.id} {...hotel} />
+                return <HotelsComp key={hotel.id} spinner={spinner} {...hotel} />
             })}
         </Grid>
     </Flex>
